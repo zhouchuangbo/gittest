cb566a50fb91d9567452cd00aa3e8a05e3d57d2b
zhouchuangbo <980661562@qq.com>
2019-01-20 21:31:36 +0800

    初始化搭建品优购工程 1.工程搭建 2.实现品牌列表数据加载-测试

---
+++ pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -0,0 +1,18 @@
+package com.pinyougou.sellergoods.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.pinyougou.mapper.BrandMapper;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.sellergoods.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+@Service(interfaceClass = BrandService.class)
+public class BrandServiceImpl implements BrandService {
+    @Autowired
+    private BrandMapper brandMapper;
+
+    public List<TbBrand> queryAll() {
+        return brandMapper.queryAll();
+    }
+}
